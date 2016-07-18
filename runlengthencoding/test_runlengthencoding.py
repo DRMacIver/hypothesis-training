897fbf96f145fc4c53e610f72e2f48d6dd96302d
Florian Bruhin <me@the-compiler.org>
2016-07-18 10:46:05 +0200

    Spelling fix in test_runlengthencoding.py

--- runlengthencoding/test_runlengthencoding.py
+++ runlengthencoding/test_runlengthencoding.py
@@ -24,7 +24,7 @@ def test_encoding_never_crashes(ls):
 
 # We restrict the range of values we test against here because if you were to
 # e.g. attempt to decode [[0, 2 ** 64]] then it would try to create a list of
-# 2 ** 64 elemnets and that would be bad. So in this test we only try decoding
+# 2 ** 64 elements and that would be bad. So in this test we only try decoding
 # serialized lists with the second element being from 0 to 10 inclusive.
 @given(st.lists(st.tuples(st.integers(), st.integers(0, 10))))
 def test_decoding_never_crashes_with_small_sizes(ls):
