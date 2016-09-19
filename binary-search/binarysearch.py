57200c81a39c06de8730805cb48171947ba6841e
Nicholas J. Radcliffe <njr@StochasticSolutions.com>
2016-09-19 13:30:52 +0100

    Fixed doc strings in runlengthencoding.py and binarysearch.py.
    Also changed the formal parameter name in binary_search to match the
    doc-string and avoid clashing with 'list'.

--- binary-search/binarysearch.py
+++ binary-search/binarysearch.py
@@ -1,28 +1,28 @@
-def binary_search(list, value):
-    """If list is sorted, return the smallest index i such that
-    ls.insert(value) would still be sorted.
+def binary_search(ls, value):
+    """If ls is sorted, return the smallest index i such that
+    ls.insert(i, value) would still be sorted.
 
-    If list is not sorted, the result from this function may be any value i in
-    the range 0 <= i <= len(list)
+    If ls is not sorted, the result from this function may be any value i in
+    the range 0 <= i <= len(ls)
 
     """
-    if not list:
+    if not ls:
         return 0
-    if value > list[-1]:
-        return len(list)
-    if value <= list[0]:
+    if value > ls[-1]:
+        return len(ls)
+    if value <= ls[0]:
         return 0
     lo = 0
-    hi = len(list) - 1
+    hi = len(ls) - 1
     while lo + 1 < hi:
         mid = (lo + hi) // 2
-        pivot = list[mid]
+        pivot = ls[mid]
         if value < pivot:
             hi = mid
         elif pivot == value:
             return mid
         else:
             lo = mid
-    assert list[lo] < value
-    assert list[hi] >= value
+    assert ls[lo] < value
+    assert ls[hi] >= value
     return hi
